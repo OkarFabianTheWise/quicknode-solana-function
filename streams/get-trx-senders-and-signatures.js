a67d41b5303bca17bbe01741aa429c29f71eaea8
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2024-12-26 15:29:09 +0100

    Update get-trx-senders-and-signatures.js

--- streams/get-trx-senders-and-signatures.js
+++ streams/get-trx-senders-and-signatures.js
@@ -40,3 +40,27 @@ function main(stream) {
         return { error: error.message, stack: error.stack };
     }
 }
+
+// RESULT
+// {
+//   "transactions": [
+//     {
+//       "balanceChange": -0.000010233,
+//       "sender": "BEmUSjqs7mpgaSXw6QdrePfTsD8aQHbdtnqUxa63La6E",
+//       "signature": "5TrLnmzw2JdRqgfsMN6pKpRk7ZSVwGGJNGaQx7Vu8T45YQKa7rBJwrcjnbnpTFaWq4kqMKiVAQ7WE4V9QCbC69Xh"
+//     },
+//     {
+//       "balanceChange": -0.000005,
+//       "sender": "7NRDm2Uqnx77dovJPY9EuWTX8VxyXsUKrESudhPwxTzY",
+//       "signature": "2YnF4CvFCvVYoJHQsbUkN6sz3TWXrMmaXAGedRAqaA9WfRM6ACZFAnb5GnujNZWdC9diQmZuHTR3Z7TY2gRCmW1g"
+//     },
+//     {
+//       "balanceChange": -0.000005,
+//       "sender": "BMJE6zFwTXwPopVdG1ccYMLWWGd5aNPvm5offrqHUxXV",
+//       "signature": "2YV7zGQ2Eey6RaVtC7gVQh6xKf698sZs6EAEyKo1pfowi8a5CTHTcQyNhhS1spSbKaBj2Jn94n7PQ8Afx32SMWxq"
+//     },
+//     {
+//       "balanceChange": -0.000005,
+//       "sender": "CcJX66BQ2Y99GQNcojA4zjDSPG71NSXCRLH2CVei6h4v",
+//       "signature": "4VDLUCeKtZWuEw9Q4a3VzbvMP4xbFhAQxRzXkVwuNQCoiS124Yuvojsxro12fpmdnupPAAnUJkYZE59xU9e5QGin"
+// ..............
